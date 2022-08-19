34542752cce07a519e64b872307a22b0b430247a
AdamBoley <adamboley92@gmail.com>
2022-08-19 16:51:17 +0000

    add admin class methods to mass disapprove comments and reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from .models import Review, Comment
 
+
 @admin.register(Review)
 class BeerReviewAdmin(admin.ModelAdmin):
 
     list_filter = ('approved', 'timestamp', 'brewery', 'type', 'author')
     list_display = ('name', 'type', 'brewery', 'timestamp', 'approved', 'author')
     search_fields = ['name', 'brewery', 'type', 'colour', 'hops', 'alcohol', 'content']
-    actions = ['approve_beer_review']
+    actions = ['approve_beer_review', 'disapprove_beer_review']
 
     def approve_beer_review(self, request, queryset):
         queryset.update(approved=True)
+
+    def disapprove_beer_review(self, request, queryset):
+        queryset.update(approved=False)
 
 
 @admin.register(Comment)
@@ -18,7 +22,10 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('approved', 'timestamp', 'review', 'author')
     list_display = ('review', 'author', 'content', 'timestamp', 'approved')
     search_fields = ['review', 'author', 'approved']
-    actions = ['approve_comment']
+    actions = ['approve_comment', 'disapprove_comment']
 
     def approve_comment(self, request, queryset):
         queryset.update(approved=True)
+
+    def diapprove_comment(self, request, queryset):
+        queryset.update(approved=False)
