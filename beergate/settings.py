994baa335766b12e842d66eaeb11d17b0b9b970f
AdamBoley <adamboley92@gmail.com>
2022-09-09 11:12:01 +0000

    reenable tinymce js root setting, add disable collectstatic config var

--- beergate/settings.py
+++ beergate/settings.py
@@ -180,6 +180,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # TinyMCE config
 
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "tinymce/tinymce.min.js")
-# TINYMCE_JS_ROOT = 'https://res.cloudinary.com/adamboley/raw/upload/v1662656035/static/tinymce/tinymce.min.f303c2d13b1e.js'
+TINYMCE_JS_ROOT = 'https://res.cloudinary.com/adamboley/raw/upload/v1662656035/static/tinymce/tinymce.min.f303c2d13b1e.js'
 # TINYMCE_COMPRESSOR = False
 
