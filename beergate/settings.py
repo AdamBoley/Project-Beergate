5fa08d82fccdf61f76e89d9a52cac241488208f1
AdamBoley <adamboley92@gmail.com>
2022-09-11 11:14:56 +0000

    remove media_root from settings

--- beergate/settings.py
+++ beergate/settings.py
@@ -171,7 +171,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATICFILES_FINDERS = [
@@ -184,4 +183,3 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
